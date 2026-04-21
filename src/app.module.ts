3ff51a47674447f0ffecdd82398ea585001b6ce5
Alberto <betooxx.dev@gmail.com>
2026-04-20 20:03:48 -0600

    feat: scaffold base structure with config, common utilities and docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,35 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { envs } from '@config/index';
+import { LoggerMiddleware } from '@common/middleware/logger.middleware';
+import { BlogsModule } from '@/modules/blogs/blogs.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      ssl: envs.stage === 'prod',
+      extra: {
+        ssl: envs.stage === 'prod' ? { rejectUnauthorized: false } : false,
+      },
+      type: 'postgres',
+      host: envs.dbHost,
+      port: envs.dbPort,
+      database: envs.dbName,
+      username: envs.dbUser,
+      password: envs.dbPassword,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    BlogsModule,
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/common/index.ts
@@ -0,0 +1,10 @@
+export { AllExceptionsFilter } from './filters/http-exception.filter';
+export { TransformInterceptor } from './interceptors/transform.interceptor';
+export type {
+  ApiResponse,
+  PaginationMeta,
+  PaginatedResponse,
+  PaginatedResult,
+} from './interfaces/api-response.interface';
+export { DEFAULT_PER_PAGE } from './constants';
+export { PaginationQueryDto } from './dto';
